0fbe42b40f16509076f37b44618694ab6c450910
wangxinyuan <1735267615@qq.com>
2017-02-28 18:49:37 +0800

    添加工具类

---
+++ app/src/main/java/cn/ucai/live/utils/MFGT.java
@@ -0,0 +1,51 @@
+package cn.ucai.live.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+
+import cn.ucai.live.I;
+import cn.ucai.live.R;
+import cn.ucai.live.ui.activity.LoginActivity;
+import cn.ucai.live.ui.activity.MainActivity;
+import cn.ucai.live.ui.activity.RegisterActivity;
+
+
+/**
+ * Created by clawpo on 2017/1/10.
+ */
+
+public class MFGT {
+    public static void finish(Activity activity){
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+    }
+
+    public static void startActivity(Activity context, Class<?> clz){
+        context.startActivity(new Intent(context, clz));
+        context.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+
+    public static void startActivity(Activity context, Intent intent){
+        context.startActivity(intent);
+        context.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+
+    public static void gotoLogin(Activity activity){
+        startActivity(activity, LoginActivity.class);
+    }
+
+    public static void gotoLoginCleanTask(Activity activity){
+        startActivity(activity, new Intent(activity,LoginActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    public static void gotoRegister(Activity activity){
+        startActivity(activity, RegisterActivity.class);
+    }
+
+    public static void gotoMain(Activity activity){
+        startActivity(activity,new Intent(activity,MainActivity.class)
+        .putExtra(I.BACK_MAIN_FROM_CHAT,true));
+    }
+}
---
+++ app/src/main/java/cn/ucai/live/utils/OnCompleteListener.java
@@ -0,0 +1,9 @@
+package cn.ucai.live.utils;
+
+
+/**
+ * Created by Administrator on 2017/1/11 0011.
+ */
+
+public interface OnCompleteListener<T> extends OkHttpUtils.OnCompleteListener<T>{
+}
