489d919e19b411336841316e351364912bea8500
wangxinyuan <1735267615@qq.com>
2017-03-02 10:00:19 +0800

    开始直播界面显示头像和昵称

--- app/src/main/java/cn/ucai/live/ui/activity/StartLiveActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/StartLiveActivity.java
@@ -30,7 +30,9 @@ import com.hyphenate.EMValueCallBack;
 import com.hyphenate.chat.EMChatRoom;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.easeui.controller.EaseUI;
+import com.hyphenate.easeui.utils.EaseUserUtils;
 import com.hyphenate.easeui.widget.EaseAlertDialog;
+import com.hyphenate.easeui.widget.EaseImageView;
 import com.ucloud.common.util.DeviceUtils;
 import com.ucloud.live.UEasyStreaming;
 import com.ucloud.live.UStreamingProfile;
@@ -39,13 +41,15 @@ import java.util.List;
 import java.util.Random;
 
 public class StartLiveActivity extends LiveBaseActivity
-    implements UEasyStreaming.UStreamingStateListener {
+        implements UEasyStreaming.UStreamingStateListener {
   private static final String TAG = StartLiveActivity.class.getSimpleName();
   @BindView(R.id.toolbar) Toolbar toolbar;
   @BindView(R.id.container) UAspectFrameLayout mPreviewContainer;
   @BindView(R.id.start_container) RelativeLayout startContainer;
   @BindView(R.id.countdown_txtv) TextView countdownView;
   @BindView(R.id.tv_username) TextView usernameView;
+  @BindView(R.id.eiv_avatar)
+  EaseImageView userAvatar;
   @BindView(R.id.btn_start) Button startBtn;
   @BindView(R.id.finish_frame) ViewStub liveEndLayout;
   @BindView(R.id.cover_image) ImageView coverImage;
@@ -81,11 +85,15 @@ public class StartLiveActivity extends LiveBaseActivity
   @Override protected void onActivityCreate(@Nullable Bundle savedInstanceState) {
     setContentView(R.layout.activity_start_live);
     ButterKnife.bind(this);
+    EaseUserUtils.setAppUserAvatar(StartLiveActivity.this,EMClient.getInstance().getCurrentUser(),
+            userAvatar);
+    EaseUserUtils.setAppUserNick(EMClient.getInstance().getCurrentUser(),
+            usernameView);
 
     liveId = TestDataRepository.getLiveRoomId(EMClient.getInstance().getCurrentUser());
     chatroomId = TestDataRepository.getChatRoomId(EMClient.getInstance().getCurrentUser());
     anchorId = EMClient.getInstance().getCurrentUser();
-    usernameView.setText(anchorId);
+   // usernameView.setText(anchorId);
     initEnv();
   }
 
