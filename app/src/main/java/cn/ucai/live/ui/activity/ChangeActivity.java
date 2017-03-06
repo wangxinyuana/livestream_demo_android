8139da06c98877693748391f860d26435b5908b4
wangxinyuan <1735267615@qq.com>
2017-03-06 16:11:04 +0800

    我的钱包界面

---
+++ app/src/main/java/cn/ucai/live/ui/activity/ChangeActivity.java
@@ -0,0 +1,17 @@
+package cn.ucai.live.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import cn.ucai.live.R;
+
+/**
+ * Created by Administrator on 2017/3/6.
+ */
+public class ChangeActivity extends BaseActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change);
+    }
+}
